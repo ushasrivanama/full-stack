4180162cdbea6ec6f422e75c319479f50f617585
ushasrivanama <114283901+ushasrivanama@users.noreply.github.com>
2024-06-15 10:03:26 +0530

    Create Book.java

---
+++ Constructors with principles,Acces Modifiers,Packages,File Handling,Collection Frame works and exception handling/Book.java
@@ -0,0 +1,31 @@
+//Create a Java class Book with a parameterized constructor that initializes title, author, and isbn. Create an object of this class and print its details.
+public class Book {
+    private String title;
+    private String author;
+    private String isbn;
+
+    public Book(String title, String author, String isbn) {
+        this.title = title;
+        this.author = author;
+        this.isbn = isbn;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public static void main(String[] args) {
+        Book book = new Book("Java Programming", "John Doe", "1234567890");
+        System.out.println("Title: " + book.getTitle());
+        System.out.println("Author: " + book.getAuthor());
+        System.out.println("ISBN: " + book.getIsbn());
+    }
+}
