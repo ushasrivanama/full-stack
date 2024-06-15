f03210d1424a28ee2c6433602a3318faecdcf016
ushasrivanama <114283901+ushasrivanama@users.noreply.github.com>
2024-06-15 09:44:52 +0530

    Create ReverseString.java

---
+++ control flow statements/ReverseString.java
@@ -0,0 +1,23 @@
+//ReverseString 
+import java.util.Scanner;
+
+public class ReverseString {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter a string: ");
+        String str = scanner.next();
+
+        String reversed = reverseString(str);
+
+        System.out.println("The reversed string is: " + reversed);
+    }
+
+    public static String reverseString(String str) {
+        String reversed = "";
+        for (int i = str.length() - 1; i >= 0; i--) {
+            reversed += str.charAt(i);
+        }
+        return reversed;
+    }
+}
