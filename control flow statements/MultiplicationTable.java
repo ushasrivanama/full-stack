92f93170f0ed12b583577c7fa770368233514491
ushasrivanama <114283901+ushasrivanama@users.noreply.github.com>
2024-06-15 09:41:59 +0530

    Create MultiplicationTable.java

---
+++ control flow statements/MultiplicationTable.java
@@ -0,0 +1,16 @@
+//Print Multiplication Table 
+import java.util.Scanner;
+
+public class MultiplicationTable {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter a number: ");
+        int num = scanner.nextInt();
+
+        System.out.println("Multiplication table of " + num + ":");
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(num + " * " + i + " = " + num * i);
+        }
+    }
+}
