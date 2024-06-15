20dab3fe3ad67075e8146a0527f1c7ca9d8fbe14
ushasrivanama <114283901+ushasrivanama@users.noreply.github.com>
2024-06-15 09:34:08 +0530

    Create SwapNumbers.java

---
+++ core java : java basics,data types,operators/SwapNumbers.java
@@ -0,0 +1,23 @@
+//SwapNumbers 
+public class SwapNumbers {
+    public static void main(String[] args) {
+        int num1 = 10;
+        int num2 = 20;
+
+        System.out.println("Before swapping: ");
+        System.out.println("num1 = " + num1);
+        System.out.println("num2 = " + num2);
+
+        swapNumbers(num1, num2);
+
+        System.out.println("After swapping: ");
+        System.out.println("num1 = " + num1);
+        System.out.println("num2 = " + num2);
+    }
+
+    public static void swapNumbers(int num1, int num2) {
+        num1 = num1 + num2;
+        num2 = num1 - num2;
+        num1 = num1 - num2;
+    }
+}
