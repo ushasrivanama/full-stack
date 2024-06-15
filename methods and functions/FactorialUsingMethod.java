d601bf36d5baa7051591aae15040f2d43345eeda
ushasrivanama <114283901+ushasrivanama@users.noreply.github.com>
2024-06-15 09:48:06 +0530

    Create FactorialUsingMethod.java

---
+++ methods and functions/FactorialUsingMethod.java
@@ -0,0 +1,23 @@
+//Factorial Using Method 
+import java.util.Scanner;
+
+public class FactorialUsingMethod {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter a number: ");
+        int num = scanner.nextInt();
+
+        long factorial = calculateFactorial(num);
+
+        System.out.println("The factorial of " + num + " is: " + factorial);
+    }
+
+    public static long calculateFactorial(int num) {
+        long factorial = 1;
+        for (int i = 1; i <= num; i++) {
+            factorial *= i;
+        }
+        return factorial;
+    }
+}
