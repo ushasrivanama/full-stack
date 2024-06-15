f0f51ccda91cdef07b4c387f3a2d899db254718d
ushasrivanama <114283901+ushasrivanama@users.noreply.github.com>
2024-06-15 09:40:29 +0530

    Create LargestAmongThree.java

---
+++ control flow statements/LargestAmongThree.java
@@ -0,0 +1,31 @@
+//Largest Among Three Numbers
+import java.util.Scanner;
+
+public class LargestAmongThree {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter the first number: ");
+        int num1 = scanner.nextInt();
+
+        System.out.println("Enter the second number: ");
+        int num2 = scanner.nextInt();
+
+        System.out.println("Enter the third number: ");
+        int num3 = scanner.nextInt();
+
+        int largest = findLargest(num1, num2, num3);
+
+        System.out.println("The largest number is: " + largest);
+    }
+
+    public static int findLargest(int num1, int num2, int num3) {
+        if (num1 >= num2 && num1 >= num3) {
+            return num1;
+        } else if (num2 >= num1 && num2 >= num3) {
+            return num2;
+        } else {
+            return num3;
+        }
+    }
+}
